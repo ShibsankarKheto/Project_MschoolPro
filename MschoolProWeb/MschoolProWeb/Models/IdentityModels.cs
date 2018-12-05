d267bbd4c8fd57aefbc2e5d337a8df1659a8fccc
Shibsankar Kheto <shibsankar1994@gmail.com>
2018-12-05 18:16:42 +0530

    Comit All for ASPNETUSER ID change string to int

--- MschoolProWeb/MschoolProWeb/Models/IdentityModels.cs
+++ MschoolProWeb/MschoolProWeb/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -6,10 +7,47 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MschoolProWeb.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+
+    // New derived classes
+    public class UserRole : IdentityUserRole<int>
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+    }
+
+    public class UserClaim : IdentityUserClaim<int>
+    {
+    }
+
+    public class UserLogin : IdentityUserLogin<int>
+    {
+    }
+
+    public class Role : IdentityRole<int, UserRole>
+    {
+        public Role() { }
+        public Role(string name) { Name = name; }
+    }
+
+    public class UserStore : UserStore<ApplicationUser, Role, int,
+        UserLogin, UserRole, UserClaim>
+    {
+        public UserStore(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+    public class RoleStore : RoleStore<Role, int, UserRole>
+    {
+        public RoleStore(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser<int, UserLogin, UserRole, UserClaim>
+    {
+        public DateTime? ActiveUntil;
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -18,10 +56,11 @@ namespace MschoolProWeb.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Role, int,
+        UserLogin, UserRole, UserClaim>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnection")
         {
         }
 
